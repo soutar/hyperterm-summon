f40fc637cb7810aa0fc39be289036162e45ca393
BuildTools <john@soutar.me>
2016-07-27 19:33:50 +0100

    Correctly handle Dock icon presses & avoid window.isVisible() in favour of window.isFocused()

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ const { dialog, globalShortcut } = require('electron');
 
 const windowSet = new Set([]);
 
+function showWindows (windows, app) {
+  windows.forEach((win, index) => {
+    if (index === windowSet.length-1) {
+      win.focus();
+    } else {
+      win.show();
+    }
+  });
+  app.show();
+}
+
+function hideWindows (windows, app) {
+  windows.forEach(win => win.hide());
+  app.hide(); // Mac OS only (re-focuses the last active app)
+}
+
 function registerSummonShortcut (app) {
   const { plugins, config } = app;
   function register (accelerator) {
     if (!accelerator) return;
     const registered = globalShortcut.register(accelerator, () => {
       const windows = [...windowSet];
-      const hiddenWindows = windows.filter(window => !window.isVisible());
-      if (hiddenWindows.length === 0) {
-        windows.forEach(win => win.hide());
-        app.hide(); // Mac OS only (re-focuses the last active app)
+      const focusedWindows = windows.filter(window => window.isFocused());
+      if (focusedWindows.length > 0) {
+        hideWindows(windows, app);
       } else {
-        windows.forEach((win, index) => {
-          if (index === windowSet.length-1) {
-            win.focus();
-          } else {
-            win.show();
-          }
-        });
+        showWindows(windows, app);
       }
     });
 
@@ -43,6 +52,10 @@ function registerSummonShortcut (app) {
       register(cfg_.summonShortcut, cfg.summonShortcut);
       cfg = cfg_;
     }
+  });
+
+  app.on('activate', () => {
+    showWindows([...windowSet], app);
   });
 
   app.on('will-quit', () => {
