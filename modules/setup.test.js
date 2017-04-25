89d1acd247b4f6bb6b289f336dce008fcab12e57
David Calhoun <dpcalhoun@gmail.com>
2017-04-25 08:07:16 -0500

    Add hideOnBlur config option

--- modules/setup.test.js
+++ modules/setup.test.js
@@ -1,18 +1,29 @@
 const setup = require('./setup');
+const windows = require('./windows');
 
+jest.mock('./windows')
 jest.mock('hyperterm-register-shortcut');
 
-let app = {
-  config: {
-    getConfig: jest.fn(() => ({}))
-  },
-  dock: {
-    hide: jest.fn()
-  },
-  on: jest.fn()
-};
+let app;
 
 describe('setup', () => {
+  beforeAll(() => {
+    app = {
+      config: {
+        getConfig: jest.fn(() => ({}))
+      },
+      dock: {
+        hide: jest.fn()
+      },
+      on: jest.fn()
+    }
+  });
+
+  afterEach(() => {
+    app.dock.hide.mockClear()
+    app.on.mockClear()
+  })
+
   describe('with default config', () => {
     beforeEach(() => {
       setup(app);
@@ -20,6 +31,10 @@ describe('setup', () => {
 
     it('does not hide the dock', () => {
       expect(app.dock.hide).not.toHaveBeenCalled();
+    });
+
+    it('handles blur events', () => {
+      expect(app.on).toHaveBeenCalledWith('browser-window-blur', expect.any(Function));
     });
   });
 
@@ -29,7 +44,7 @@ describe('setup', () => {
         summon: {
           hideDock: true
         }
-      })
+      });
       setup(app);
     });
 
@@ -37,4 +52,19 @@ describe('setup', () => {
       expect(app.dock.hide).toHaveBeenCalled();
     });
   });
-})
+
+  describe('with hideOnBlur config disabled', () => {
+    beforeEach(() => {
+      app.config.getConfig.mockReturnValueOnce({
+        summon: {
+          hideOnBlur: false
+        }
+      });
+      setup(app);
+    });
+
+    it('does not handle blur events', () => {
+      expect(app.on).not.toHaveBeenCalledWith('browser-window-blur', expect.any(Function));
+    });
+  });
+});
