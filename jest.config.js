5377905f561a526945bbcda19fc76a786b1a91f2
David Calhoun <dpcalhoun@gmail.com>
2017-10-15 11:44:24 -0500

    Add prettier

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  coveragePathIgnorePatterns: [
+    '/node_modules',
+    '<rootDir>/(fixtures)/'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/utils/test.js'
+  ]
+}
