23f1a383702a993f78c3d88e06f5ef5a51943d18
David Calhoun <dpcalhoun@gmail.com>
2018-02-25 20:32:39 -0600

    Avoid unnecessary invocations of setVisibility

--- modules/__tests__/setVisibility.test.js
+++ modules/__tests__/setVisibility.test.js
@@ -6,17 +6,47 @@ jest.useFakeTimers();
 const app = generateApp();
 
 describe('modules/setVisibility', () => {
-  it('hides the dock after a timeout', () => {
-    setVisibility(app.dock, { hide: true });
-    expect(app.dock.hide).not.toHaveBeenCalled();
-    jest.runAllTimers();
-    expect(app.dock.hide).toHaveBeenCalled();
+  describe('when no dock', () => {
+    it('does not throw error', () => {
+      expect(() => setVisibility(undefined, { hide: true })).not.toThrow();
+    });
   });
 
-  it('shows the dock after a timeout', () => {
-    setVisibility(app.dock, { hide: false });
-    expect(app.dock.show).not.toHaveBeenCalled();
-    jest.runAllTimers();
-    expect(app.dock.show).toHaveBeenCalled();
+  describe('when dock is visibile', () => {
+    it('hides the dock after a timeout', () => {
+      app.dock.isVisible.mockReturnValue(true);
+      setVisibility(app.dock, { hide: true });
+      expect(app.dock.hide).not.toHaveBeenCalled();
+      jest.runAllTimers();
+      expect(app.dock.hide).toHaveBeenCalled();
+    });
+  });
+
+  describe('when dock is hidden', () => {
+    it('shows the dock after a timeout', () => {
+      app.dock.isVisible.mockReturnValue(false);
+      setVisibility(app.dock, { hide: false });
+      expect(app.dock.show).not.toHaveBeenCalled();
+      jest.runAllTimers();
+      expect(app.dock.show).toHaveBeenCalled();
+    });
+  });
+
+  describe('when dock hidden', () => {
+    it('does not hide the dock', () => {
+      app.dock.isVisible.mockReturnValue(false);
+      setVisibility(app.dock, { hide: true });
+      jest.runAllTimers();
+      expect(app.dock.hide).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when dock visible', () => {
+    it('does not show the dock', () => {
+      app.dock.isVisible.mockReturnValue(true);
+      setVisibility(app.dock, { hide: false });
+      jest.runAllTimers();
+      expect(app.dock.show).not.toHaveBeenCalled();
+    });
   });
 });
--- modules/setVisibility.js
+++ modules/setVisibility.js
@@ -1,6 +1,18 @@
 let dockVisibilityTO;
 
 module.exports = (dock, { hide }) => {
+  if (!dock) {
+    return;
+  }
   clearTimeout(dockVisibilityTO);
-  dockVisibilityTO = setTimeout(hide ? dock.hide : dock.show, 1000);
+  switch (true) {
+    case hide && dock.isVisible():
+      dockVisibilityTO = setTimeout(dock.hide, 1000);
+      break;
+    case !hide && !dock.isVisible():
+      dockVisibilityTO = setTimeout(dock.show, 1000);
+      break;
+    default:
+      break;
+  }
 };
