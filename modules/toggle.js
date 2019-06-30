758cf2fe308ba3e17e6f2e5c777eec03fa9d3b1d
David Calhoun <dpcalhoun@gmail.com>
2019-06-30 08:29:39 -0500

    [wip] Explore pinned windows
    This rudimentary pinned windows works, but focus management is broken.
    There is likely no way to determine which unpinned window should regain focus
    without keeping an internal collection of windows.
    
    However, pinned windows will always gain focus during hide, which will break
    giving focus to the previously focused OS application. There is likely no way
    around this and makes this feature incompatible with this plugin.

--- modules/toggle.js
+++ modules/toggle.js
@@ -1,7 +1,12 @@
 const { hideWindows, showWindows } = require('./windows');
 
+// TODO: This value would have to be managed in app and passed down.
+let pinnedWindows = [{ pinned: false }, { pinned: true }, { pinned: false }];
+
 module.exports = app => {
-  const focusedWindows = [...app.getWindows()].filter(w => w.isFocused());
+  const focusedWindows = [...app.getWindows()].filter(
+    (w, i) => w.isVisible() && !pinnedWindows[i].pinned
+  );
 
   focusedWindows.length > 0 ? hideWindows(app) : showWindows(app);
 };
--- modules/windows.js
+++ modules/windows.js
@@ -15,8 +15,13 @@ exports.generateBlurCallback = (callback, app) =>
     callback(app);
   }, 100);
 
+// TODO: This value would have to be managed in app and passed down.
+let pinnedWindows = [{ pinned: false }, { pinned: true }, { pinned: false }];
+
 exports.hideWindows = app => {
-  const visibleWindows = [...app.getWindows()].filter(w => w.isVisible());
+  const visibleWindows = [...app.getWindows()].filter(
+    (w, i) => w.isVisible() && !pinnedWindows[i].pinned
+  );
 
   if (!visibleWindows.length) {
     return false;
@@ -34,17 +39,30 @@ exports.hideWindows = app => {
     }
   });
 
-  if (typeof app.hide === 'function') {
-    app.hide();
-  }
+  // TODO: Hiding the app would nullify pinned windows and would need to be
+  // conditionally called.
+  // if (typeof app.hide === 'function') {
+  //   app.hide();
+  // }
 };
 
+// NOTE: This rudimentary pinned windows works, but focus management is broken.
+// There is likely no way to determine which unpinned window should regain focus
+// without keeping an internal collection of windows.
+//
+// However, pinned windows will always gain focus during hide, which will break
+// giving focus to the previously focused OS application. There is likely no way
+// around this and makes this feature incompatible with this plugin.
 exports.showWindows = app => {
-  const windows = [...app.getWindows()].sort(isFocused.bind(app));
+  const windows = [...app.getWindows()]
+    .filter((w, i) => !pinnedWindows[i].pinned)
+    .sort(isFocused.bind(app));
 
   windows.length === 0 ? app.createWindow() : windows.forEach(w => w.show());
 
-  if (typeof app.show === 'function') {
-    app.show();
-  }
+  // TODO: Hiding the app would nullify pinned windows and would need to be
+  // conditionally called.
+  // if (typeof app.show === 'function') {
+  //   app.show();
+  // }
 };
